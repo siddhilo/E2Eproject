97b0b22c7bb9618ac04d06e962b7c2edc937e1e3
Siddhi <siddhi.surange@gmail.com>
2020-05-05 00:17:00 +0530

    first commit

---
+++ src/main/java/com/poison/App.java
@@ -0,0 +1,13 @@
+package com.poison;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
