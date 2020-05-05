7f690195a833f12f4edfc1480f1cfc95037dd3d3
siddhilo <59177893+siddhilo@users.noreply.github.com>
2020-05-05 21:18:10 +0530

    mycommit

---
+++ src/test/java/com/poison/Demo1Test.java
@@ -0,0 +1,25 @@
+package com.poison;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class Demo1Test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Check Assertion");
+        assertTrue( true );
+        System.out.println("codingatBranch2");
+        System.out.println("codingatBranch2");
+        System.out.println("codingatBranch2");
+        System.out.println("codingatBranch2");
+    }
+}
